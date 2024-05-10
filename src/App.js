65927739fa660fdeae8d25a1b2a982136a55b57f
Umesh Das <dasumesh2002@gmail.com>
2024-05-10 11:30:58 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-
 import "./App.css"
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import useClipboard from "react-use-clipboard";
-import {useState} from "react";
-
+import { useState } from "react";
+import { IoTrashBin } from 'react-icons/io5';
 
 const App = () => {
-    const [textToCopy, setTextToCopy] = useState();
+    const [textToCopy, setTextToCopy] = useState("");
     const [isCopied, setCopied] = useClipboard(textToCopy, {
-        successDuration:1000
+        successDuration: 1000
     });
 
-    //subscribe to thapa technical for more awesome videos
+    const { transcript, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
 
-    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
-    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+    const startListening = () => {
+        SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+        setTextToCopy(transcript);
+    };
+
+    const stopListening = () => {
+        SpeechRecognition.stopListening();
+        setTextToCopy(transcript);
+    };
+
+    const resetTranscriptAndCopy = () => {
+        resetTranscript();
+        setTextToCopy("");
+    };
 
     if (!browserSupportsSpeechRecognition) {
         return null
@@ -24,26 +35,26 @@ const App = () => {
         <>
             <div className="container">
                 <h2>Speech to Text Converter</h2>
-                <br/>
+                <br />
                 <p>A React hook that converts speech from the microphone to text and makes it available to your React
                     components.</p>
 
-                <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
+                <div className="main-content" onClick={() => setTextToCopy(transcript)}>
                     {transcript}
+                    <div className="reset-btn" onClick={resetTranscriptAndCopy}>
+                        <IoTrashBin />
+                    </div>
                 </div>
 
                 <div className="btn-style">
-
                     <button onClick={setCopied}>
                         {isCopied ? 'Copied!' : 'Copy to clipboard'}
                     </button>
                     <button onClick={startListening}>Start Listening</button>
-                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
-
+                    <button onClick={stopListening}>Stop Listening</button>
                 </div>
 
             </div>
-
         </>
     );
 };
